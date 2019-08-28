9f1907911bb416339f3a41206a08b213e5864860
raigekieraven <emiljulioortiz@outlook.com>
2019-08-28 22:30:22 +0800

    dialogue system
    -finished making dialogue system
    -might need to test for bugs

--- Assets/Scripts/DialogueManagerNew.cs
+++ Assets/Scripts/DialogueManagerNew.cs
@@ -23,11 +23,13 @@ public class DialogueManagerNew : MonoBehaviour
     public Text dialogueName;
     public Text dialogueText;
     public Image dialoguePortrait;
+    public float delay = 0.001f;
 
     public Queue<DialogueBase.Info> dialogueInfo = new Queue<DialogueBase.Info>();
 
     public void EnqueueDialogue(DialogueBase db)
     {
+        dialogueBox.SetActive(true);
         dialogueInfo.Clear();
 
         foreach(DialogueBase.Info info in db.dialogueInfo)
@@ -51,6 +53,20 @@ public class DialogueManagerNew : MonoBehaviour
         dialogueName.text = info.myName;
         dialogueText.text = info.myText;
         dialoguePortrait.sprite = info.portrait;
+
+        StartCoroutine(TypeText(info));
+    }
+
+    IEnumerator TypeText(DialogueBase.Info info)
+    {
+        dialogueText.text = "";
+
+        foreach(char c in info.myText.ToCharArray())
+        {
+            yield return new WaitForSeconds(delay);
+            dialogueText.text += c;
+            yield return null;
+        }
     }
 
     public void EndofDialogue()
