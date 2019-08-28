2836f1fabad6b7cce2255608709d0a2e9a093dd5
raigekieraven <43919756+raigekieraven@users.noreply.github.com>
2019-08-28 18:52:06 +0800

    dialogue system
    -almost done with the dialogue system

--- Assets/Scripts/DialogueManagerNew.cs
+++ Assets/Scripts/DialogueManagerNew.cs
@@ -5,7 +5,56 @@ using UnityEngine.UI;
 
 public class DialogueManagerNew : MonoBehaviour
 {
+    public static DialogueManagerNew instance;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Fix this" + gameObject.name);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject dialogueBox;
+
     public Text dialogueName;
     public Text dialogueText;
     public Image dialoguePortrait;
+
+    public Queue<DialogueBase.Info> dialogueInfo = new Queue<DialogueBase.Info>();
+
+    public void EnqueueDialogue(DialogueBase db)
+    {
+        dialogueInfo.Clear();
+
+        foreach(DialogueBase.Info info in db.dialogueInfo)
+        {
+            dialogueInfo.Enqueue(info);
+        }
+
+        DequeueDialogue();
+    }
+
+    public void DequeueDialogue()
+    {
+        if(dialogueInfo.Count == 0)
+        {
+            EndofDialogue();
+            return;
+        }
+
+        DialogueBase.Info info = dialogueInfo.Dequeue();
+
+        dialogueName.text = info.myName;
+        dialogueText.text = info.myText;
+        dialoguePortrait.sprite = info.portrait;
+    }
+
+    public void EndofDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
 }
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public DialogueBase dialogue;
+
+    public void TriggerDialogue()
+    {
+        DialogueManagerNew.instance.EnqueueDialogue(dialogue);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TriggerDialogue();
+        }
+    }
+}
