c3bd04dc37e2eecc96fd9a78f774dfe82499f2ec
richard <chaaaardkun@gmail.com>
2019-08-31 04:13:58 +0800

    placed all maps and rooms as well as placed the npc in their respective positions. added a teleport to room for going inside

--- Assets/Scripts/EnterDoor.cs
+++ Assets/Scripts/EnterDoor.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class EnterDoor : MonoBehaviour
 {
-    public string level;
+    public Transform player;
+    public float x,y;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(level);
+        player.transform.position = new Vector2(x,y);
     }
 }
