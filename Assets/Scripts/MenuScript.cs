ead5b761b7a05d89cd88c6d7f89bde82fa316a91
raigekieraven <43919756+raigekieraven@users.noreply.github.com>
2019-08-31 10:31:44 +0800

    colliders
    - added several colliders
    - added a menu scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public void NextScene()
+    {
+        Debug.Log("Next scene");
+    }
+}
