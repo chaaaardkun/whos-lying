b7f9a76baf32d4eccee3c10abd2358113d5c600e
richard <chaaaardkun@gmail.com>
2019-08-31 12:31:23 +0800

    added the main menu scene

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("hallway scene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You quit the game");
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Scripts/Volume_Change.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volume_Change : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+   
+}
